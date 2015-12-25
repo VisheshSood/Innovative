9af3542aa07c32ac053d58b2130631f6ebd97494
VisheshSood <omg.itsvishesh@gmail.com>
2015-12-25 14:21:29 +0700

    website update

--- html_form_send.php
+++ html_form_send.php
@@ -2,8 +2,8 @@
 if(isset($_POST['email'])) {
 	
 	// CHANGE THE TWO LINES BELOW
-	$email_to = "omg.itsvishesh@gmail.com, vsood@uw.edu";
-	
+	$email_to = "rsood1963@gmail.com";
+
 	$email_subject = "Website Comment!";
 	
 	
@@ -17,7 +17,6 @@ if(isset($_POST['email'])) {
 	
 	// validation expected data exists
 	if(!isset($_POST['first_name']) ||
-		!isset($_POST['last_name']) ||
 		!isset($_POST['email']) ||
 		!isset($_POST['telephone']) ||
 		!isset($_POST['comments'])) {
@@ -25,7 +24,6 @@ if(isset($_POST['email'])) {
 	}
 	
 	$first_name = $_POST['first_name']; // required
-	$last_name = $_POST['last_name']; // required
 	$email_from = $_POST['email']; // required
 	$telephone = $_POST['telephone']; // not required
 	$comments = $_POST['comments']; // required
@@ -38,9 +36,6 @@ if(isset($_POST['email'])) {
 	$string_exp = "/^[A-Za-z .'-]+$/";
   if(!preg_match($string_exp,$first_name)) {
   	$error_message .= 'The First Name you entered does not appear to be valid.<br />';
-  }
-  if(!preg_match($string_exp,$last_name)) {
-  	$error_message .= 'The Last Name you entered does not appear to be valid.<br />';
   }
   if(strlen($comments) < 2) {
   	$error_message .= 'The Comments you entered do not appear to be valid.<br />';
@@ -56,7 +51,6 @@ if(isset($_POST['email'])) {
 	}
 	
 	$email_message .= "First Name: ".clean_string($first_name)."\n";
-	$email_message .= "Last Name: ".clean_string($last_name)."\n";
 	$email_message .= "Email: ".clean_string($email_from)."\n";
 	$email_message .= "Telephone: ".clean_string($telephone)."\n";
 	$email_message .= "Comments: ".clean_string($comments)."\n";
@@ -64,12 +58,13 @@ if(isset($_POST['email'])) {
 	
 // create email headers
 $headers = 'From: '.$email_from."\r\n".
+
+$headers .= "BCC: omg.itsvishesh@gmail.com".PHP_EOL;
 'Reply-To: '.$email_from."\r\n" .
 'X-Mailer: PHP/' . phpversion();
 @mail($email_to, $email_subject, $email_message, $headers);  
 ?>
 
-<!-- place your own success html below -->
 
 Thank you for contacting Innovative Gloves. We will be in touch with you very soon.
 
